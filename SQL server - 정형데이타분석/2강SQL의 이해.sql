65ec09f6e22d4b772775615f1a061cef8281e8f7
tomatojams <tomatojams@naver.com>
2023-09-11 04:03:28 +0900

    change

---
+++ SQL server - 정형데이타분석/2강SQL의 이해.sql
@@ -0,0 +1,7 @@
+insert into HumanResources.Department (Name, GroupName, ModifiedDate)
+VALUES ('DATA SCIENCE2', 'Research and Development','2023-02-14')
+
+select * from HumanResources.Department
+
+update HumanResources.Department set Name ='DATA ANALIST' WHERE Name = 'DATA SCIENCE2'
+
